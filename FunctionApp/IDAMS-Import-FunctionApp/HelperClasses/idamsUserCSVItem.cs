2171c2459cb5504a24cd2c8bee95600b82e343b5
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-06-15 11:11:37 +0100

    updates

--- FunctionApp/IDAMS-Import-FunctionApp/HelperClasses/idamsUserCSVItem.cs
+++ FunctionApp/IDAMS-Import-FunctionApp/HelperClasses/idamsUserCSVItem.cs
@@ -13,13 +13,13 @@ namespace IDAMS_Import_FunctionApp.HelperClasses
             [CsvColumn(Name = "uid")]
             public string uid { get; set; }
 
-            [CsvColumn(Name = "Name")]
+            [CsvColumn(Name = "name")]
             public string name { get; set; }
 
-            [CsvColumn(Name = "UPIN")]
+            [CsvColumn(Name = "upin")]
             public string upin { get; set; }
 
-            [CsvColumn(Name = "UKPRN")]
+            [CsvColumn(Name = "ukprn")]
             public string ukprn { get; set; }
 
             [CsvColumn(Name = "Superuser")]
