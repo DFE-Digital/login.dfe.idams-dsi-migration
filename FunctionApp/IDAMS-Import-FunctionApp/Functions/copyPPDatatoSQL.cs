43c4b043643dbf61c4946e1fce27c5bd6a70b06b
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-07-11 11:56:56 +0100

    updates

--- FunctionApp/IDAMS-Import-FunctionApp/Functions/copyPPDatatoSQL.cs
+++ FunctionApp/IDAMS-Import-FunctionApp/Functions/copyPPDatatoSQL.cs
@@ -42,7 +42,7 @@ namespace IDAMS_Import_FunctionApp.Functions
 
             while (true)
             {
-                recordNumber++;
+                
                 string uri = Environment.GetEnvironmentVariable("PP_API_ENDPOINT_URL") + "?limit=" +limit + "&offset=" + offset;
                 var webRequest = (HttpWebRequest)WebRequest.Create(uri);
                 webRequest.Method = "GET";
@@ -60,6 +60,7 @@ namespace IDAMS_Import_FunctionApp.Functions
 
                     foreach (JObject obj in arr)
                     {
+                        recordNumber++;
                         masterProviderCode = obj.Value<string>("masterProviderCode") ?? "";
                         upin = obj.Value<string>("upin") ?? "";
                         pimsProviderType = obj.Value<string>("pimsProviderType") ?? "";
@@ -72,24 +73,26 @@ namespace IDAMS_Import_FunctionApp.Functions
                         masterUkprn = obj.Value<string>("masterUkprn") ?? "";
                         giasUrn = obj.Value<string>("giasUrn") ?? "";
                         masterEdubaseUid = obj.Value<string>("masterEdubaseUid") ?? "";
+
+                        log.LogInformation($"------Record Start-----");
+                        log.LogInformation($"Page Number : {pageNumber}");
+                        log.LogInformation($"Record Number: {recordNumber}");
+                        log.LogInformation($"upin: {upin}");
+                        log.LogInformation($"pimsProviderType: {pimsProviderType}");
+                        log.LogInformation($"pimsStatus: {pimsStatus}");
+                        log.LogInformation($"districtAdministrativeName: {districtAdministrativeName}");
+                        log.LogInformation($"masterDateOpened: {masterDateOpened}");
+                        log.LogInformation($"sourceSystem: {sourceSystem}");
+                        log.LogInformation($"masterProviderTypeName: {masterProviderTypeName}");
+                        log.LogInformation($"giasProviderType: {giasProviderType}");
+                        log.LogInformation($"masterUkprn: {masterUkprn}");
+                        log.LogInformation($"Master Provider Code: {masterProviderCode}");
+                        log.LogInformation($"giasUrn: {giasUrn}");
+                        log.LogInformation($"masterEdubaseUid: {masterEdubaseUid}");
+                        log.LogInformation($"------Record End-----");
                     }
                 }
-                log.LogInformation($"------Record Start-----");
-                log.LogInformation($"Page Number : {pageNumber}");
-                log.LogInformation($"Record Number: {recordNumber}");
-                log.LogInformation($"upin: {upin}");
-                log.LogInformation($"pimsProviderType: {pimsProviderType}");
-                log.LogInformation($"pimsStatus: {pimsStatus}");
-                log.LogInformation($"districtAdministrativeName: {districtAdministrativeName}");
-                log.LogInformation($"masterDateOpened: {masterDateOpened}");
-                log.LogInformation($"sourceSystem: {sourceSystem}");
-                log.LogInformation($"masterProviderTypeName: {masterProviderTypeName}");
-                log.LogInformation($"giasProviderType: {giasProviderType}");
-                log.LogInformation($"masterUkprn: {masterUkprn}");
-                log.LogInformation($"Master Provider Code: {masterProviderCode}");
-                log.LogInformation($"giasUrn: {giasUrn}");
-                log.LogInformation($"masterEdubaseUid: {masterEdubaseUid}");
-                log.LogInformation($"------Record End-----");
+               
 
                 offset += limit;
                 pageNumber+=1;
