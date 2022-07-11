b407c2834ceefa9bff7b18ba4f3f8425ed062d51
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-07-11 11:23:18 +0100

    updates

--- FunctionApp/IDAMS-Import-FunctionApp/Functions/copyPPDatatoSQL.cs
+++ FunctionApp/IDAMS-Import-FunctionApp/Functions/copyPPDatatoSQL.cs
@@ -22,12 +22,27 @@ namespace IDAMS_Import_FunctionApp.Functions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = "copyPPtoSQL";        
+            string name = "copyPPtoSQL";
+            int recordNumber = 0;
             var limit = 1000;
             var offset = 0;
+            int pageNumber = 1;
             string masterProviderCode = "";
+            string upin = "";
+            string pimsProviderType = "";
+            string pimsStatus = "";
+            string districtAdministrativeName = "";
+            string masterDateOpened = "";
+            string sourceSystem = "";
+            string masterProviderTypeName = "";
+            string giasProviderType = "";
+            string masterUkprn = "";
+            string giasUrn = "";
+            string masterEdubaseUid = "";
+
             while (true)
             {
+                recordNumber++;
                 string uri = Environment.GetEnvironmentVariable("PP_API_ENDPOINT_URL") + "?limit=" +limit + "&offset=" + offset;
                 var webRequest = (HttpWebRequest)WebRequest.Create(uri);
                 webRequest.Method = "GET";
@@ -45,12 +60,39 @@ namespace IDAMS_Import_FunctionApp.Functions
 
                     foreach (JObject obj in arr)
                     {
-                         masterProviderCode = obj.Value<string>("masterProviderCode") ?? "";
+                        masterProviderCode = obj.Value<string>("masterProviderCode") ?? "";
+                        upin = obj.Value<string>("upin") ?? "";
+                        pimsProviderType = obj.Value<string>("pimsProviderType") ?? "";
+                        pimsStatus = obj.Value<string>("pimsStatus") ?? "";
+                        districtAdministrativeName = obj.Value<string>("districtAdministrativeName") ?? "";
+                        masterDateOpened = obj.Value<string>("masterDateOpened") ?? "";
+                        sourceSystem = obj.Value<string>("sourceSystem") ?? "";
+                        masterProviderTypeName = obj.Value<string>("masterProviderTypeName") ?? "";
+                        giasProviderType = obj.Value<string>("giasProviderType") ?? "";
+                        masterUkprn = obj.Value<string>("masterUkprn") ?? "";
+                        giasUrn = obj.Value<string>("giasUrn") ?? "";
+                        masterEdubaseUid = obj.Value<string>("masterEdubaseUid") ?? "";
                     }
                 }
-
+                log.LogInformation($"------Record Start-----");
+                log.LogInformation($"Page Number : {pageNumber}");
+                log.LogInformation($"Record Number: {recordNumber}");
+                log.LogInformation($"upin: {upin}");
+                log.LogInformation($"pimsProviderType: {pimsProviderType}");
+                log.LogInformation($"pimsStatus: {pimsStatus}");
+                log.LogInformation($"districtAdministrativeName: {districtAdministrativeName}");
+                log.LogInformation($"masterDateOpened: {masterDateOpened}");
+                log.LogInformation($"sourceSystem: {sourceSystem}");
+                log.LogInformation($"masterProviderTypeName: {masterProviderTypeName}");
+                log.LogInformation($"giasProviderType: {giasProviderType}");
+                log.LogInformation($"masterUkprn: {masterUkprn}");
                 log.LogInformation($"Master Provider Code: {masterProviderCode}");
+                log.LogInformation($"giasUrn: {giasUrn}");
+                log.LogInformation($"masterEdubaseUid: {masterEdubaseUid}");
+                log.LogInformation($"------Record End-----");
+
                 offset += limit;
+                pageNumber+=1;
             }
             string responseMessage = string.IsNullOrEmpty(name)
                     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
