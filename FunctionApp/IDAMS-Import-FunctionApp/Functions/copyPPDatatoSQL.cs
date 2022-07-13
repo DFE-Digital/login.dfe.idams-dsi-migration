214397786342011d1a585b5b08751567a6c5da87
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-07-13 11:14:33 +0100

    updates

--- FunctionApp/IDAMS-Import-FunctionApp/Functions/copyPPDatatoSQL.cs
+++ FunctionApp/IDAMS-Import-FunctionApp/Functions/copyPPDatatoSQL.cs
@@ -27,7 +27,7 @@ namespace IDAMS_Import_FunctionApp.Functions
 
             string name = "copyPPtoSQL";
             int recordNumber = 0;
-            var limit = 2000;
+            var limit = 1000;
             var offset = 0;
             int pageNumber = 1;
             JArray arr = null;
