67dc491d7c3b1274cf2860ade24185a92999ffb3
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-07-14 09:28:40 +0100

    updates

--- SQLScripts/StoredProcedures/sp_PPJSONDataMerge.sql
+++ SQLScripts/StoredProcedures/sp_PPJSONDataMerge.sql
@@ -6,24 +6,45 @@ AS
 BEGIN
 	--This top line essentially does a "SELECT *" from the named table
 	--and looks for a match based on the "ON" statement below
-	MERGE dbo.organisation AS Target
-	USING @pp_org_type AS Source
-		ON source.masterUkprn = Target.UKPRN
-		OR source.giasUrn = Target.URN
-		OR source.upin = Target.UID
-			-- For Updates
+	MERGE INTO dbo.organisation AS TGT
+	USING (
+		SELECT masterUkprn
+			,giasUrn
+			,upin
+			,pimsStatus
+			,districtAdministrativeName
+			,masterDateOpened
+			,sourceSystem
+			,masterProviderTypeName
+			,giasProviderType
+			,masterprovidercode
+			,pimsProviderType
+		FROM @pp_org_type
+		GROUP BY masterUkprn
+			,giasUrn
+			,upin
+			,pimsStatus
+			,districtAdministrativeName
+			,masterDateOpened
+			,sourceSystem
+			,masterProviderTypeName
+			,giasProviderType
+			,masterprovidercode
+			,pimsProviderType
+		) AS SRC
+		ON SRC.masterUkprn = TGT.UKPRN
+			OR SRC.giasUrn = TGT.URN
+			OR SRC.upin = TGT.UID
 	WHEN MATCHED
 		THEN
 			UPDATE
-			SET Target.ProviderProfileID = Source.masterprovidercode
-				,Target.UPIN = Source.upin
-				,Target.PIMSProviderType = Source.pimsProviderType
-				,Target.PIMSStatus = Source.pimsStatus
-				,Target.DistrictAdministrativeName = Source.districtAdministrativeName
-				,Target.OpenedOn = Source.masterDateOpened
-				,Target.SourceSystem = Source.sourceSystem 
-				,Target.ProviderTypeName = Source.masterProviderTypeName 
-				,Target.GiasProviderType = Source.giasProviderType ;				
-				
-	
+			SET TGT.ProviderProfileID = SRC.masterprovidercode
+				,TGT.UPIN = SRC.upin
+				,TGT.PIMSProviderType = SRC.pimsProviderType
+				,TGT.PIMSStatus = SRC.pimsStatus
+				,TGT.DistrictAdministrativeName = SRC.districtAdministrativeName
+				,TGT.OpenedOn = SRC.masterDateOpened
+				,TGT.SourceSystem = SRC.sourceSystem
+				,TGT.ProviderTypeName = SRC.masterProviderTypeName
+				,TGT.GiasProviderType = SRC.giasProviderType;
 END
