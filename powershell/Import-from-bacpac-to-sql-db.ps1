d8ea92f067b57e85d17fc0a2604b2715c90f79a8
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-07-21 11:14:34 +0100

    Update Import-from-bacpac-to-sql-db.ps1

--- powershell/Import-from-bacpac-to-sql-db.ps1
+++ powershell/Import-from-bacpac-to-sql-db.ps1
@@ -26,6 +26,41 @@ param (
     $adminpwd
      
 )
+function Cancel-AzSQLImportExportOperation
+{
+    param
+    (
+        [parameter(Mandatory=$true)][string]$resourceGroupName
+        ,[parameter(Mandatory=$true)][string]$serverName
+        ,[parameter(Mandatory=$true)][string]$databaseName
+    )
+
+    $Operation = Get-AzSqlDatabaseActivity -ResourceGroupName $ResourceGroupName -ServerName $ServerName -DatabaseName $DatabaseName | Where-Object {($_.Operation -like "Export*" -or $_.Operation -like "Import*") -and $_.State -eq "InProgress"}
+    
+    if(-not [string]::IsNullOrEmpty($Operation))
+    {
+        do
+        {
+            Write-Host -ForegroundColor Cyan ("Operation " + $Operation.Operation + " with OperationID: " + $Operation.OperationId + " is now " + $Operation.State)
+            $UserInput = Read-Host -Prompt "Should I cancel this operation? (Y/N)"
+        } while($UserInput -ne "Y" -and $UserInput -ne "N")
+
+        if($UserInput -eq "Y")
+        { 
+            "Canceling operation"
+            Stop-AzSqlDatabaseActivity -ResourceGroupName $ResourceGroupName -ServerName $ServerName -DatabaseName $DatabaseName -OperationId $Operation.OperationId
+        }
+        else 
+        {"Exiting without cenceling the operation"}
+        
+    }
+    else
+    {
+        "No import or export operation is now running"
+    }
+}
+
+Cancel-AzSQLImportExportOperation
 $secureString = convertto-securestring $adminpwd -asplaintext -force
 # Import bacpac to database with an S3 performance level
 $importRequest = New-AzSqlDatabaseImport -ResourceGroupName $resourceGroupName `
