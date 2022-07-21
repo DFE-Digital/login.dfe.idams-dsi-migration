eee68e95532f95ea479d75e67c49a4afc3d6d8a5
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-07-21 11:24:21 +0100

    Update Import-from-bacpac-to-sql-db.ps1

--- powershell/Import-from-bacpac-to-sql-db.ps1
+++ powershell/Import-from-bacpac-to-sql-db.ps1
@@ -60,5 +60,29 @@ function Cancel-AzSQLImportExportOperation
     }
 }
 
-Cancel-AzSQLImportExportOperation -resourceGroupName $resourceGroupName -serverName $serverName -databaseName $databaseName
+Cancel-AzSQLImportExportOperation -resourceGroupName $resourceGroupName -serverName 's141d02-signin-shd-sql' -databaseName 'd02-testorgs01'
+$secureString = convertto-securestring $adminpwd -asplaintext -force
+# Import bacpac to database with an S3 performance level
+$importRequest = New-AzSqlDatabaseImport -ResourceGroupName $resourceGroupName `
+    -ServerName 's141d02-signin-shd-sql' `
+    -DatabaseName 'd02-testorgs01' `
+    -DatabaseMaxSizeBytes 100GB `
+    -StorageKeyType "StorageAccessKey" `
+    -StorageKey $(Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName).Value[0] `
+    -StorageUri "https://$storageaccountname.blob.core.windows.net/$storageContainerName/$bacpacFilename" `
+    -Edition "Standard" `
+    -ServiceObjectiveName "S3" `
+    -AdministratorLogin "$adminSqlLogin" `
+    -AdministratorLoginPassword $(ConvertTo-SecureString -String $secureString -AsPlainText -Force)
 
+# Check import status and wait for the import to complete
+$importStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
+[Console]::Write("Importing")
+while ($importStatus.Status -eq "InProgress")
+{
+    $importStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
+    [Console]::Write(".")
+    Start-Sleep -s 10
+}
+[Console]::WriteLine("")
+$importStatus
