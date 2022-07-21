9e4cb1f036f710efab0a7af1ad65b416139e0145
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-07-21 11:07:29 +0100

    Update Import-from-bacpac-to-sql-db.ps1

--- powershell/Import-from-bacpac-to-sql-db.ps1
+++ powershell/Import-from-bacpac-to-sql-db.ps1
@@ -28,10 +28,6 @@ param (
 )
 $secureString = convertto-securestring $adminpwd -asplaintext -force
 # Import bacpac to database with an S3 performance level
-try
-{
-
-
 $importRequest = New-AzSqlDatabaseImport -ResourceGroupName $resourceGroupName `
     -ServerName 's141d02-signin-shd-sql' `
     -DatabaseName 'd02-testorgs' `
@@ -55,7 +51,3 @@ while ($importStatus.Status -eq "InProgress")
 }
 [Console]::WriteLine("")
 $importStatus
-}
-catch {
-    throw "An error occurred: $($_.Exception.Message)"
-}
