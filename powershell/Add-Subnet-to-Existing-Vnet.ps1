0763b2b41edfff02fde197c7afae1d227de4a0fe
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-10-11 11:01:15 +0100

    updates

--- powershell/Add-Subnet-to-Existing-Vnet.ps1
+++ powershell/Add-Subnet-to-Existing-Vnet.ps1
@@ -17,13 +17,6 @@ param (
      
 )
 
-
-$vNetName = 'global_vnet_eastus'
-$resourceGroupName = 'vcloud-lab.com'
-$AddressPrefix = @('10.10.0.0/16')
-$subnetName = 'prod01-10.10.1.x'
-
-
 #Get existing Azure Virtual Network information
 $azvNet = Get-AzVirtualNetwork -Name $vNetName -ResourceGroupName $resourceGroupName
 Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnet01AddressPrefix -VirtualNetwork $azvNet 
