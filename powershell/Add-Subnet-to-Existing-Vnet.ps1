16109a8cb00c42b40ccaaba9c46426ebfd1d7cab
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-10-11 11:38:30 +0100

    Update Add-Subnet-to-Existing-Vnet.ps1

--- powershell/Add-Subnet-to-Existing-Vnet.ps1
+++ powershell/Add-Subnet-to-Existing-Vnet.ps1
@@ -12,13 +12,10 @@ param (
     [Parameter(Mandatory = $true)]
     [string]
     $subnetName
-
-    
-     
 )
 #Get existing Azure Virtual Network information
 $azvNet = Get-AzVirtualNetwork -Name $vNetName -ResourceGroupName $resourceGroupName
-Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnet01AddressPrefix -VirtualNetwork $azvNet 
+Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $AddressPrefix -VirtualNetwork $azvNet 
 
 #Make changes to vNet
 $azvNet | Set-AzVirtualNetwork
