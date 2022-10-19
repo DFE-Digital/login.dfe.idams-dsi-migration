38b0a9d4add4bf22e5e8a64d7fbf6c1692286b6b
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-10-19 21:36:23 +0100

    updates

---
+++ powershell/Create-Workflow-And-Connectors.ps1
@@ -0,0 +1,51 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $applicationResourceGroupName,    
+    [Parameter(Mandatory = $true)]
+    [string]
+    $StorageAccountName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $idamsdsifileshare,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $logicappname,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $artifactName
+)
+
+$accountKey = (Get-AzStorageAccountKey -ResourceGroupName $applicationResourceGroupName -Name $StorageAccountName)[0].Value
+$ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $accountKey
+$s = Get-AzStorageShare $idamsdsifileshare -Context $ctx
+
+Write-Host -ForegroundColor Green "Creating directory in file share.."    
+if (!$s.ShareClient.GetRootDirectoryClient().GetFileClient("workflow.json").Exists()) {
+    Get-AzStorageShare -Context $ctx -Name $idamsdsifileshare | New-AzStorageDirectory -Path "/site/wwwroot/$logicappname"  
+}
+Set-Location "$(Pipeline.Workspace)/$artifactName/"
+$CurrentFolder = (Get-Item .).FullName
+$files = Get-ChildItem -Recurse | Where-Object { $_.GetType().Name -eq "FileInfo" }
+
+foreach ($file in $files) {
+    $path = $file.FullName.Substring($Currentfolder.Length + 1).Replace("\", "/")
+    $path = "scripts/" + $path 
+              
+    try {
+        if ($file.FullName.Contains("workflow")) {
+            Write-output "Writing: $($file.FullName)" 
+            Set-AzStorageFileContent -Share $s.CloudFileShare -Source $file.FullName -Path "/site/wwwroot/$logicappname" -Force
+        }
+        if ($file.FullName.Contains("connections") -Or $file.FullName.Contains("parameters")) {
+            Write-output "Writing: $($file.FullName)" 
+            Set-AzStorageFileContent -Share $s.CloudFileShare -Source $file.FullName -Path "/site/wwwroot/" -Force
+        }
+
+    }
+    catch {
+        $message = $_
+        Write-Warning "That didn't work: $message"
+    }
+}
