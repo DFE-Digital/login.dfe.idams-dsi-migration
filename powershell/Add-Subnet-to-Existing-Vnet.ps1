814f0872766c0b21b7b679f065470a67b93eb4d5
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-10-21 10:49:58 +0100

    updates to the import function for vnet

--- powershell/Add-Subnet-to-Existing-Vnet.ps1
+++ powershell/Add-Subnet-to-Existing-Vnet.ps1
@@ -15,19 +15,23 @@ param (
 )
 
 #Get existing Azure Virtual Network information
+$subnetNameArray = $subnetName.Split(",")
+$addressPrefixArray = $AddressPrefix.Split(",")
 $azvNet = Get-AzVirtualNetwork -Name $vNetName -ResourceGroupName $resourceGroupName
-$existingsubnet = Get-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $azvNet -ErrorAction SilentlyContinue
+For ($i = 0; $i -lt $subnetNameArray.Length; $i++) {
+   
+    $existingsubnet = Get-AzVirtualNetworkSubnetConfig -Name $subnetNameArray[$i] -VirtualNetwork $azvNet -ErrorAction SilentlyContinue
 
-if(!$existingsubnet)
-{
-    #Get existing service endpoints
-    $ServiceEndPoint = New-Object 'System.Collections.Generic.List[String]'
-    $ServiceEndPoint.Add("Microsoft.Storage")
-    #$azvNet.ServiceEndpoints | ForEach-Object { $ServiceEndPoint.Add($_.service) }
+    if (!$existingsubnet) {
+        #Get existing service endpoints
+        $ServiceEndPoint = New-Object 'System.Collections.Generic.List[String]'
+        $ServiceEndPoint.Add("Microsoft.Storage")
+        $ServiceEndPoint.Add("Microsoft.Web")
+        
+        Add-AzVirtualNetworkSubnetConfig -Name $subnetNameArray[$i] -AddressPrefix $addressPrefixArray[$i] -VirtualNetwork $azvNet -ServiceEndpoint $ServiceEndPoint
 
-    Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $AddressPrefix -VirtualNetwork $azvNet -ServiceEndpoint $ServiceEndPoint
+        #Make changes to vNet
+        $azvNet | Set-AzVirtualNetwork
 
-    #Make changes to vNet
-    $azvNet | Set-AzVirtualNetwork
-
+    }
 }
