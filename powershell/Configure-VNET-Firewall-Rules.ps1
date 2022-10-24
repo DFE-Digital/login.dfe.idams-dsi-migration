dc93958c29e91c0cdb6dda003d9f51f0574ee2f7
zeeshansGitHub <67509023+zeeshansGitHub@users.noreply.github.com>
2022-10-24 11:18:12 +0100

    updates

--- powershell/Configure-VNET-Firewall-Rules.ps1
+++ powershell/Configure-VNET-Firewall-Rules.ps1
@@ -32,10 +32,14 @@ param (
     $subnetNamefa
 )
 
+
 $subnet = Get-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name $vNetName | Get-AzVirtualNetworkSubnetConfig -Name $subnetName
 $vnetResouceId = "/subscriptions/"+$subscriptionId+"/resourceGroups/"+$resourceGroupName+"/providers/Microsoft.Network/virtualNetworks/"+$vNetName
+
+Write-Host "Integrate Storage Account to the Vnet"
 
 Add-AzStorageAccountNetworkRule -ResourceGroupName $storageaccountRGName -Name $storageaccountName -VirtualNetworkResourceId $subnet.Id
 az storage account update --name $storageaccountName --resource-group $storageaccountRGName --default-action Deny
 Write-Host "Integrate Function App to the Vnet"
+
 az functionapp  vnet-integration add --resource-group $functionAppRG --name $functionAppName --vnet $vnetResouceId --subnet $subnetNamefa
