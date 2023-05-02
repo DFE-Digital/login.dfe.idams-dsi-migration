e20ac154ead9fa82751aaa476bfc9ff4e2dddc2e
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-05-02 12:18:47 +0100

    updates

--- SQLScripts/StoredProcedures/sp_AddUpdateIDAMSUserServicesRoles.sql
+++ SQLScripts/StoredProcedures/sp_AddUpdateIDAMSUserServicesRoles.sql
@@ -1,9 +1,4 @@
-/****** Object:  StoredProcedure [dbo].[sp_AddUpdateIDAMSUserServicesRoles]    Script Date: 14/03/2023 21:54:52 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-ALTER PROCEDURE [dbo].[sp_AddUpdateIDAMSUserServicesRoles] (
+CREATE PROCEDURE [dbo].[sp_AddUpdateIDAMSUserServicesRoles] (
 	-- Add the parameters for the stored procedure here
 	@idams_user_type IDAMS_USER_TYPE readonly
 	)
@@ -12,24 +7,24 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON
-DECLARE @IDAMSUserRolesData  IDAMS_USER_TYPE
-	INSERT INTO @IDAMSUserRolesData
+	DECLARE @IDAMSUserData  IDAMS_USER_TYPE
+	INSERT INTO @IDAMSUserData
 	SELECT * FROM @idams_user_type
 
-	;WITH cteIDAMSRoles
+	;WITH cteIDAMS
 	AS (
-		SELECT mail,roleName
+		SELECT mail,ukprn
 			,Row_number() OVER (
-				PARTITION BY mail,roleName ORDER BY mail,roleName
+				PARTITION BY mail,serviceId,roleName ORDER BY mail,serviceId,roleName
 				) row_num
-		FROM @IDAMSUserRolesData
+		FROM @IDAMSUserData
 		)
 	DELETE
-	FROM cteIDAMSRoles
+	FROM cteIDAMS
 	WHERE row_num > 1;
 	-- Insert statements for procedure here
 	MERGE dbo.idams_user_services_roles AS Target
-	USING @IDAMSUserRolesData AS Source
+	USING @IDAMSUserData AS Source
 		ON source.mail  = Target.mail
 			AND (
 				SELECT perian_serviceName
