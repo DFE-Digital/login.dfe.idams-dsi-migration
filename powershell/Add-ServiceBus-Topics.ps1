3268ba44644298d50db3c2f3b9b55e310b3c6aa2
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-05-16 14:14:59 +0100

    updates

--- powershell/Add-ServiceBus-Topics.ps1
+++ powershell/Add-ServiceBus-Topics.ps1
@@ -15,11 +15,11 @@ param (
     $serviceBusSubscriptionName
     
 )
-Write-Host "Create Topic"
+Write-Host "Create Topic - providerprofilesync"
 az servicebus topic create --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --name $serviceBusTopicName
 
-Write-Host "Create Subscription - ppcachestatus-messages"
-az servicebus topic subscription create --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --topic-name $serviceBusTopicName --name 'ppcachestatus-messages'
+Write-Host "Create Subscriptions for Topic - providerprofilesync"
+
 Write-Host "Create Subscription - ppsyncstatus-messages"
 az servicebus topic subscription create --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --topic-name $serviceBusTopicName --name 'ppsyncstatus-messages'
 Write-Host "Create Subscription - ppdeltacollect-messages"
@@ -28,3 +28,10 @@ Write-Host "Create Subscription - ppdeltateachers-messages"
 az servicebus topic subscription create --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --topic-name $serviceBusTopicName --name 'ppdeltateachers-messages'
 Write-Host "Create Subscription - ppdeltas2s-messages"
 az servicebus topic subscription create --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --topic-name $serviceBusTopicName --name 'ppdeltas2s-messages'
+
+Write-Host "Create Topic - providerprofilecache"
+az servicebus topic create --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --name 'providerprofilecache'
+
+Write-Host "Create Subscriptions for Topic - providerprofilecache"
+Write-Host "Create Subscriptions - ppcachestatus-messages"
+az servicebus topic subscription create --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --topic-name 'providerprofilecache' --name 'ppcachestatus-messages'
