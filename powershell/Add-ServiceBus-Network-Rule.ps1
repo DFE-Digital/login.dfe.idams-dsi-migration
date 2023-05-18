664d5b7d1c4c313c1714ad7561d2d17abdb326c3
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-05-18 12:12:04 +0100

    Update Add-ServiceBus-Network-Rule.ps1

--- powershell/Add-ServiceBus-Network-Rule.ps1
+++ powershell/Add-ServiceBus-Network-Rule.ps1
@@ -9,11 +9,17 @@ param (
     $serviceBusNamespace,
     [Parameter(Mandatory = $true)]
     [string]
-    $subnetName,
+    $subnetFunctionName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $subnetStorageName,
     [Parameter(Mandatory = $true)]
     [string]
     $vnetName
     
 )
 Write-Host "Create Network Rule to allow connections from Delta Functions"
-az servicebus namespace network-rule add --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --subnet $subnetName --vnet-name $vnetName
+az servicebus namespace network-rule add --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --subnet $subnetFunctionName --vnet-name $vnetName
+
+Write-Host "Create Network Rule to allow connections from Delta Storage Accounts"
+az servicebus namespace network-rule add --resource-group $serviceBusResourceGroupName --namespace-name $serviceBusNamespace --subnet $subnetStorageName --vnet-name $vnetName
