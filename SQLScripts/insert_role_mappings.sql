5268bf72046d94b8f2d77d0a1ffbd86215af0689
jo-joy <jovin.joy@outlook.com>
2023-07-20 15:31:14 +0100

    role updates

--- SQLScripts/insert_role_mappings.sql
+++ SQLScripts/insert_role_mappings.sql
@@ -14,53 +14,45 @@ INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [ac
 GO
 INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (7, N'MYESF - Contract Authoriser', N'Submit subcontractor declarations', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (8, N'MYESF - Contract Authoriser', N'View apprenticeship grant for employers reports', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (8, N'MYESF - Contract Manager', N'View contracts and agreements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (9, N'MYESF - Contract Manager', N'View contracts and agreements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (9, N'MYESF - Contract Manager', N'View funding claims and reconciliation statements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (10, N'MYESF - Contract Manager', N'View funding claims and reconciliation statements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (10, N'MYESF - Contract Manager', N'View allocation statements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (11, N'MYESF - Contract Manager', N'View allocation statements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (11, N'MYESF - Contract Manager', N'View previous subcontractor declarations', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (12, N'MYESF - Contract Manager', N'View previous subcontractor declarations', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (12, N'MYESF - Contract Manager', N'Submit subcontractor declarations', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (13, N'MYESF - Contract Manager', N'Submit subcontractor declarations', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (13, N'MYESF - Contract User', N'View contracts and agreements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (14, N'MYESF - Contract Manager', N'View apprenticeship grant for employers reports', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (14, N'MYESF - Contract User', N'View funding claims and reconciliation statements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (15, N'MYESF - Contract User', N'View contracts and agreements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (15, N'MYESF - Contract User', N'View allocation statements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (16, N'MYESF - Contract User', N'View funding claims and reconciliation statements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (16, N'MYESF - Contract User', N'View previous subcontractor declarations', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (17, N'MYESF - Contract User', N'View allocation statements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (17, N'MYESF - Data Returns and Claims Authoriser', N'View funding claims and reconciliation statements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (18, N'MYESF - Contract User', N'View previous subcontractor declarations', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (18, N'MYESF - Data Returns and Claims Authoriser', N'Sign funding claims', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (19, N'MYESF - Contract User', N'View apprenticeship grant for employers reports', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (19, N'MYESF - Data Returns and Claims Authoriser', N'View allocation statements', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (20, N'MYESF - Data Returns and Claims Authoriser', N'View funding claims and reconciliation statements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (20, N'MYESF - Data Returns and Claims Authoriser', N'View previous subcontractor declarations', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (21, N'MYESF - Data Returns and Claims Authoriser', N'Sign funding claims', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (21, N'MYESF - Data Returns and Claims Authoriser', N'Submit subcontractor declarations', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (22, N'MYESF - Data Returns and Claims Authoriser', N'View allocation statements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (22, N'Payment Viewer', N'View payment history', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (23, N'MYESF - Data Returns and Claims Authoriser', N'View previous subcontractor declarations', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (23, N'MYESF - View as Provider', N'View as Provider', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (24, N'MYESF - Data Returns and Claims Authoriser', N'Submit subcontractor declarations', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (24, N'MYESF - Download SCD Reports', N'Download SCD Reports', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (25, N'MYESF - Data Returns and Claims Authoriser', N'View apprenticeship grant for employers reports', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (25, N'MYESF - Download Reports', N'Download Reports', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (26, N'Payment Viewer', N'View payment history', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (26, N'MYESF - Admin', N'MYESF Admin', 1)
 GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (27, N'MYESF - View as Provider', N'View as Provider', 1)
-GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (28, N'MYESF - Download SCD Reports', N'Download SCD Reports', 1)
-GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (29, N'MYESF - Download Reports', N'Download Reports', 1)
-GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (30, N'MYESF - Admin', N'MYESF Admin', 1)
-GO
-INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (31, N'MYESF - Data Returns and Claims Authoriser', N'ViewContractsAndAgreements', 1)
+INSERT [dbo].[idams_role_mapping] ([Id], [idams_role_name], [dsi_role_name], [active]) VALUES (27, N'MYESF - Data Returns and Claims Authoriser', N'ViewContractsAndAgreements', 1)
 GO
 SET IDENTITY_INSERT [dbo].[idams_role_mapping] OFF
 GO
