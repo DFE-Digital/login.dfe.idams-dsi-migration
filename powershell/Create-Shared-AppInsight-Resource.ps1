43771f412b4edeb44e5664fc96907167afaf4de8
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-07-31 21:55:44 +0100

    Update Create-Shared-AppInsight-Resource.ps1

--- powershell/Create-Shared-AppInsight-Resource.ps1
+++ powershell/Create-Shared-AppInsight-Resource.ps1
@@ -15,10 +15,5 @@ param (
 
 Write-Host "Creating Shared App Insights Resource for Pirean"
 
-az resource create \"
-    --resource-group $resourceGroupName \
-    --resource-type "Microsoft.Insights/components" \
-    --name $applicationInsightsName \
-    --location location \
-    --properties '{"Application_Type":"web"}'"
+az resource create \" --resource-group $resourceGroupName \--resource-type "Microsoft.Insights/components" \--name $applicationInsightsName \--location location \--properties '{"Application_Type":"web"}'"
 
