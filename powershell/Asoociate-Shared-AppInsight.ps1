38e0b9a220a7b065ef82add43806eb59a01c00ee
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-08-01 12:40:37 +0100

    Update Asoociate-Shared-AppInsight.ps1

--- powershell/Asoociate-Shared-AppInsight.ps1
+++ powershell/Asoociate-Shared-AppInsight.ps1
@@ -16,12 +16,9 @@ param (
    
 )
 
-$appInsightsKey=$(az monitor app-insights component show --resource-group $appInsightsResourceGroupName --name $appInsightsName --query 'instrumentationKey' -o tsv)
-
+# $appInsightsKey=$(az monitor app-insights component show --resource-group $appInsightsResourceGroupName --name $appInsightsName --query 'instrumentationKey' -o tsv)
+$appInsightsKey = $(az resource show -g $appInsightsResourceGroupName -n $appInsightsName --resource-type "microsoft.insights/components" --query properties.InstrumentationKey)
+                  
 Write-Host "Associate Shared App Insight with the Import Function"
 
-# az functionapp config appsettings set --name $functionAppName --resource-group $functionAppResourceGroupName --settings "APPINSIGHTS_INSTRUMENTATIONKEY=$appInsightsKey"
-az functionapp config set --name $functionAppName --resource-group $functionAppResourceGroupName --app-settings "APPINSIGHTS_INSTRUMENTATIONKEY=$appInsightsKey"
-
-
-
+az functionapp config appsettings set --name $functionAppName --resource-group $functionAppResourceGroupName --settings "APPINSIGHTS_INSTRUMENTATIONKEY=$appInsightsKey"
