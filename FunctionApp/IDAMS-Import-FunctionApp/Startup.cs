1e8ca1615ce7f2d6d115e3a916af42bcf671f24e
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-08-02 11:16:11 +0100

    updates for app insight

---
+++ FunctionApp/IDAMS-Import-FunctionApp/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+[assembly: FunctionsStartup(typeof(IDAMS_Import_FunctionApp.Startup))]
+namespace IDAMS_Import_FunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            // Configure Application Insights
+            var instrumentationKey = "16650b26-5638-47f6-aeee-c4bcf18fb00d";
+            var config = TelemetryConfiguration.Active;
+            config.InstrumentationKey = instrumentationKey;
+        }
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Configuration and services setup go here
+        }
+    }
+}
