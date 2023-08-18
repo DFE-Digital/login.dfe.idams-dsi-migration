0200e939cd7339392f4b540ef58d1246875ad438
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-08-18 18:14:26 +0100

    updates

--- powershell/Create-PrivateLinkScope-For-AppInsight-Resource.ps1
+++ powershell/Create-PrivateLinkScope-For-AppInsight-Resource.ps1
@@ -12,7 +12,10 @@ param (
     $applicationInsightsName,
     [Parameter(Mandatory = $true)]
     [string]
-    $scopeName
+    $scopeName,
+    [Parameter(Mandatory = $true)]
+    [string]
+    $environmentName
    
 )
 # Get the resource ID of the App Insight Resource and save it in a variable
@@ -25,4 +28,5 @@ az monitor private-link-scope  create --name $scopeName --resource-group $resour
 
 Write-Host "Associate Private Link Scope for AppInsight Resource"
 
-az monitor private-link-scope update --name $scopeName --resource-group $resourceGroupName --private-link-service Connections --associated-resource $resourceid
+az monitor private-link-scope update --name $scopeName --resource-group $resourceGroupName --private-link-service Connections --associated-resource $resourceid --tags Environment=$environmentName Product="DfE Sign-in" Service Offering="DfE Sign-in"
+   
