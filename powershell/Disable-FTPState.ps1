d68e595fbf46006de7679f0ed60c3bf0e202b4b4
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-11-20 18:59:29 +0000

    Update Disable-FTPState.ps1

--- powershell/Disable-FTPState.ps1
+++ powershell/Disable-FTPState.ps1
@@ -12,9 +12,9 @@ param (
 )
                   
 Write-Host "Disable Function FTP-State"
-# $jsonPayload = @{
-#     ftpsState = "Disabled"
-# } | ConvertTo-Json
-#az functionapp config set --resource-group $resourceGroupName --name $functionAppName --generic-configurations $jsonPayload
- az functionapp config set --resource-group $resourceGroupName --name $functionAppName --generic-configurations {ftpsState:Disabled}
+$jsonPayload = @{
+    'ftpsState' = 'Disabled'
+} | ConvertTo-Json
+az functionapp config set --resource-group $resourceGroupName --name $functionAppName --generic-configurations $jsonPayload
+# az functionapp config set --resource-group $resourceGroupName --name $functionAppName --generic-configurations {ftpsState:Disabled}
 
