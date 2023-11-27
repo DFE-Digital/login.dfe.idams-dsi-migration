4d4e2f3c413846ab1adc1b4f2f6dca8f563ce821
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-11-27 16:53:52 +0000

    updates for pirean infra

--- powershell/Disable-FTPState.ps1
+++ powershell/Disable-FTPState.ps1
@@ -1,18 +1,35 @@
-[CmdletBinding()]
-param (
+# [CmdletBinding()]
+# param (
     
-    [Parameter(Mandatory = $true)]
-    [string]
-    $resourceGroupName,
-    [Parameter(Mandatory = $true)]
-    [string]
-    $functionAppName
+#     [Parameter(Mandatory = $true)]
+#     [string]
+#     $resourceGroupName,
+#     [Parameter(Mandatory = $true)]
+#     [string]
+#     $functionAppName
 
    
-)
+# )
                   
-Write-Host "Disable Function FTP-State"
-# $jsonPayload = @{"ftpsState" = "Disabled"} | ConvertTo-Json
+# Write-Host "Disable Function FTP-State"
+# # $jsonPayload = @{"ftpsState" = "Disabled"} | ConvertTo-Json
 
-az functionapp config set --resource-group $resourceGroupName --name $functionAppName --ftps-state Disabled
+# az functionapp config set --resource-group $resourceGroupName --name $functionAppName --ftps-state Disabled
 
+
+# Replace this value with the specific string you're looking for in the function app name
+$targetString = "pirean"
+
+# Get a list of function apps containing the target string
+$functionApps = Get-AzWebApp | Where-Object { $_.Name -like "*$targetString*" } | Select-Object -ExpandProperty Name
+
+# Loop through each function app
+foreach ($functionApp in $functionApps) {
+    $resourceGroup = (Get-AzResource -ResourceId $functionApp.ResourceId).ResourceGroupName
+    Write-Host "Processing function app: $functionApp in resource group: $resourceGroup"
+
+    # Update the FTP state of the function app (replace 'AllAllowed' with your actual logic)
+    Set-AzWebApp -ResourceGroupName $resourceGroup -Name $functionApp -FtpsState "Disabled"
+    
+    Write-Host "FTP state updated for function app: $functionApp"
+}
