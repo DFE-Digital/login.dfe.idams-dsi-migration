faab1a1a1d7755f54d4ce4da5b24ffdde26ffa9b
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-11-27 17:19:15 +0000

    updates for infra

--- powershell/Disable-FTPState.ps1
+++ powershell/Disable-FTPState.ps1
@@ -1,23 +1,4 @@
-# [CmdletBinding()]
-# param (
-    
-#     [Parameter(Mandatory = $true)]
-#     [string]
-#     $resourceGroupName,
-#     [Parameter(Mandatory = $true)]
-#     [string]
-#     $functionAppName
-
-   
-# )
-                  
-# Write-Host "Disable Function FTP-State"
-# # $jsonPayload = @{"ftpsState" = "Disabled"} | ConvertTo-Json
-
 # az functionapp config set --resource-group $resourceGroupName --name $functionAppName --ftps-state Disabled
-
-
-# Replace this value with the specific string you're looking for in the function app name
 $targetString = "pirean"
 
 # Get a list of function apps containing the target string
